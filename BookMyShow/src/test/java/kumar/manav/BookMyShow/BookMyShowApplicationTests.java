2c6507badd86c2f97c507de6936ae6aba902d6f7
Manav <manavk1301@gmail.com>
2024-05-11 18:59:27 +0530

    init

---
+++ BookMyShow/src/test/java/kumar/manav/BookMyShow/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package kumar.manav.BookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
