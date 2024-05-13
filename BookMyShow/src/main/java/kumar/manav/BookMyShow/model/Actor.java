28ba80e6d8645f4075198df1933c16913b048d43
Manav <manavk1301@gmail.com>
2024-05-13 22:14:19 +0530

    adding models after this

---
+++ BookMyShow/src/main/java/kumar/manav/BookMyShow/model/Actor.java
@@ -0,0 +1,13 @@
+package kumar.manav.BookMyShow.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Actor extends BaseModel{
+
+
+}
---
+++ BookMyShow/src/main/java/kumar/manav/BookMyShow/model/BaseModel.java
@@ -0,0 +1,26 @@
+package kumar.manav.BookMyShow.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+    @Column(name = "UPDATED_BY")
+    private String updatedBy;
+    @Column(name = "UPDATED_AT")
+    private String updatedAt;
+}
